cb9e43767e6a7e02e495753be53cb2399addbebb
Devendra Yadav <devendrainfotech@gmail.com>
2025-03-23 16:50:03 +0530

    Update Vite configuration for base path and adjust router history mode to hash

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../components/Home.vue'
 import AboutUs from '../components/AboutUs.vue'
 import Contact from '../components/Contact.vue'
@@ -22,7 +22,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
